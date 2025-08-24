71ca13c2a917cf09bc984de0d1b2b0a0ed9bffe6
praise <praisemogaji@gmail.com>
2025-08-24 08:20:16 -0700

    Final Changes

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-import pickle
+from utils import make_prediction
 
-cv = pickle.load(open("models/cv.pkl", "rb"))
-clf = pickle.load(open("models/clf.pkl", "rb"))
 
 app = Flask(__name__)
 
@@ -13,19 +11,16 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
     email = request.form.get("content")
-    tokenized_email = cv.transform([email])
-    predictions = clf.predict(tokenized_email)
-    predictions = 1 if predictions == 1 else -1
+    predictions = make_prediction(email)
     return render_template("index.html", predictions=predictions, email=email)
 
-@app.route("/api/predict", methods=["POST"])
-def api_predict():
-    data = request.get_json(force=True)
-    email = data["content"]
-    tokenized_email = cv.transform([email])
-    predictions = clf.predict(tokenized_email)
-    predictions = 1 if predictions == 1 else -1
-    return jsonify({predictions: predictions})
+@app.route('/api/predict', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)  # Get data posted as a json
+    email = data['content']
+    prediction = make_prediction(email)
+    return jsonify({'prediction': prediction, 'email': email})  # Return prediction
+
 
 if __name__ == "__main__":
     app.run(debug=True)
